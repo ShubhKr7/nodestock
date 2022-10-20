3afe760f69a0c69405bcb44d0d1de5262c4e3d07
Shubhankar Tiwari <geniusshubhankartiwari@gmail.com>
2022-10-20 15:19:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const  exphbs = require('express-handlebars');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs.engine());
+const otherstuff = "Hello there this is other stuff...";
+
+// Set Handlebars route
+app.get('/', function (req, res)  {
+    res.render('home', {
+    	stuff: otherstuff
+    });
+});
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('server Listening to port '+PORT));
